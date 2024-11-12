da3402b3b18445e8c7640c06b659b6d290469526
Elvin0515 <delisimsek07gmail.com>
2024-11-13 00:50:46 +0400

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,46 @@
 import React from "react";
-
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      {" "}
+      <div className="NavBar">
+        {" "}
+        <ul>
+          {" "}
+          <li>
+            {" "}
+            <h3>JobStart</h3>{" "}
+          </li>{" "}
+          <div className="spaceBetweenNavItems"></div>{" "}
+          <li>
+            {" "}
+            <p className="text-black">Home</p>{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            <p className="infoOverHover">Category</p>{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            <p className="shadeOverHover">Blog</p>{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            <p className="shadeOverHover">About</p>{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            <p className="shadeOverHover">Contact</p>{" "}
+          </li>{" "}
+          <li>
+            {" "}
+            <button type="button" class="btn btn-info text-white">
+              + Post A Job
+            </button>{" "}
+          </li>{" "}
+        </ul>{" "}
+      </div>{" "}
+    </div>
+  );
 };
-
 export default Header;
