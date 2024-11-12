2c7a1dc672989f43a64ddded983e9acd7edb5adb
turanlz <turanhasan013@gmail.com>
2024-11-12 06:59:54 +0400

    Banner Slider Finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
-import Home from './pages/Home/Home';
+import Home from "./pages/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
     <>
-     <Home/>
+      <Home />
     </>
-  )
-
+  );
 }
 
 export default App;
