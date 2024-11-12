8f4c527320195fa89822b8cd6b17eb65c4e7694c
turanlz <turanhasan013@gmail.com>
2024-11-12 05:23:17 +0400

    Job Card Finish

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../../css/Home.css";
 import HeroBg from "../../components/HomeBanner/HeroBg";
 import RecentJobs from "../../components/RecentJobs/RecentJobs";
+import JobCard from "../../components/JobCard/JobCard";
 
 const Home = () => {
   return (
     <div className='main'>
       <HeroBg />
       <RecentJobs />
+      <JobCard />
     </div>
   );
 };
