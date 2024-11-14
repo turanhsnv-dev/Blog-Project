8655238064ced1cab2ff3b422d0500420640e2cb
Elvin0515 <delisimsek07gmail.com>
2024-11-14 21:02:06 +0400

    layout, routes,

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-// import Contact from "./Contact";
-import Layout from "./Layout";
-import About from "./About";
-import Blog from "./Blog";
-import Category from "./Category";
+import Home from "./pages/Home/Home";
+import Layout from "./components/Layout";
+import About from "./pages/About/About";
 
 function App() {
   return (
@@ -15,14 +12,12 @@ function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/contact" element={<Contact />} /> */}
             <Route path="/About" element={<About />} />
-            {/* <Route path="/blog" element={<Blog />} /> */}
-            {/* <Route path="/category" element={<Category />} /> */}
           </Route>
         </Routes>
       </Router>
     </div>
   );
 }
+
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,31 +10,22 @@ const Header = () => {
             <h3>JobStart</h3>
           </li>
           <div className="spaceBetweenNavItems"></div>
-          <li>
-            <NavLink to="/" className="text-black pgf">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="Category" className="infoOverHover pgf">
-              Category
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="Blog" className="shadeOverHover pgf">
-              Blog
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="About" className="shadeOverHover pgf">
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="Contact" className="shadeOverHover pgf">
-              Contact
-            </NavLink>
-          </li>
+          <NavLink to="/" className="text-black pgf">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="shadeOverHover pgf">
+            About
+          </NavLink>
+          <NavLink to="/category" className="infoOverHover pgf">
+            Category
+          </NavLink>
+          <NavLink to="/blog" className="shadeOverHover pgf">
+            Blog
+          </NavLink>
+          <NavLink to="/contact" className="shadeOverHover pgf">
+            Contact
+          </NavLink>
+
           <li>
             <button type="button" className="btn btn-info text-white">
               + Post A Job
