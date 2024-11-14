1be768025471fb0a33f01b373323d4b458a44196
Elvin0515 <delisimsek07gmail.com>
2024-11-14 19:36:55 +0400

    head

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,38 @@
 import React from "react";
+
 const Header = () => {
   return (
     <div>
-      {" "}
       <div className="NavBar">
-        {" "}
         <ul>
-          {" "}
           <li>
-            {" "}
-            <h3>JobStart</h3>{" "}
-          </li>{" "}
-          <div className="spaceBetweenNavItems"></div>{" "}
+            <h3>JobStart</h3>
+          </li>
+          <div className="spaceBetweenNavItems"></div>
           <li>
-            {" "}
-            <p className="text-black">Home</p>{" "}
-          </li>{" "}
+            <p className="text-black">Home</p>
+          </li>
           <li>
-            {" "}
-            <p className="infoOverHover">Category</p>{" "}
-          </li>{" "}
+            <p className="infoOverHover">Category</p>
+          </li>
           <li>
-            {" "}
-            <p className="shadeOverHover">Blog</p>{" "}
-          </li>{" "}
+            <p className="shadeOverHover">Blog</p>
+          </li>
           <li>
-            {" "}
-            <p className="shadeOverHover">About</p>{" "}
-          </li>{" "}
+            <p className="shadeOverHover">About</p>
+          </li>
           <li>
-            {" "}
-            <p className="shadeOverHover">Contact</p>{" "}
-          </li>{" "}
+            <p className="shadeOverHover">Contact</p>
+          </li>
           <li>
-            {" "}
-            <button type="button" class="btn btn-info text-white">
+            <button type="button" className="btn btn-info text-white">
               + Post A Job
-            </button>{" "}
-          </li>{" "}
-        </ul>{" "}
-      </div>{" "}
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
+
 export default Header;
