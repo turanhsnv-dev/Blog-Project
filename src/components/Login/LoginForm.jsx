df99c56930a86578a6a6bbaaa15fc0b361f6c8b7
rufik111 <huseynovrufet140@gmail.com>
2024-11-26 21:17:46 +0400

    register method

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "../../css/LoginForm.css";
-
+import RegisterForm from "./RegisterForm";
 function LoginForm() {
   const [isRegistering, setIsRegistering] = useState(false);
+
 
   return (
     <div className="container py-5 my-5 d-flex justify-content-center align-items-center">
@@ -10,46 +11,7 @@ function LoginForm() {
         <div className="loginContainer d-flex justify-content-center align-items-center flex-column">
           {isRegistering ? (
             <>
-              <h2 className="fw-bold">Sign up</h2>
-              <form className="flex-column py-4 d-flex justify-content-center align-items-center">
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  className="mt-3 p-2"
-                />
-                <input
-                  type="text"
-                  name="surname"
-                  placeholder="Surname"
-                  className="mt-3 p-2"
-                />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  className="mt-3 p-2"
-                />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  className="my-4 p-2"
-                />
-                <select className="fw-bold">
-                  <option value="Your Role">Your Role</option>
-                  <option value="FrontEnd Developer">FrontEnd Developer</option>
-                  <option value="BackEnd Developer">BackEnd Developer</option>
-                  <option value="FullStack Developer">
-                    FullStack Developer
-                  </option>
-                  <option value="Designer">Designer</option>
-                  <option value="Web Security">Web Security</option>
-                </select>
-                <button className="btn fw-bold text-white py-2 mt-3">
-                  Sign up
-                </button>
-              </form>
+              <RegisterForm/>
             </>
           ) : (
             <>
