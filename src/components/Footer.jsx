cca8627af799f5f10e06e618095d9e61a7978156
Elvin0515 <delisimsek07gmail.com>
2024-11-30 23:34:46 +0400

    media for the comonents that does not have responsive design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,49 +4,59 @@ const Footer = () => {
   return (
     <div className="bg-shaded">
       <div className="container">
-        <div className="row py-5 opacity">
+        <div className="footer-content row py-5 opacity">
           <div className="col-sm-2 text-white">
             <h5>For Candidates</h5>
+            <hr />
             <p className="whiteOverHover">Register</p>
             <p className="whiteOverHover">Find Jobs</p>
             <p className="whiteOverHover">News</p>
             <p className="whiteOverHover">Search Jobs</p>
             <p className="whiteOverHover">Contact</p>
             <p className="whiteOverHover">Careers</p>
+            <hr />
           </div>
           <div className="col-sm-2 text-white">
             <h5>For Employers</h5>
+            <hr />
             <p className="whiteOverHover">Employer Account</p>
             <p className="whiteOverHover">Clients</p>
             <p className="whiteOverHover">News</p>
             <p className="whiteOverHover">Find Candidates</p>
             <p className="whiteOverHover">Terms & Policies</p>
             <p className="whiteOverHover">Careers</p>
+            <hr />
           </div>
           <div className="col-sm-2 text-white">
             <h5>Archives</h5>
+            <hr />
             <p className="whiteOverHover">January 2018</p>
             <p className="whiteOverHover">February 2018</p>
             <p className="whiteOverHover">March 2018</p>
             <p className="whiteOverHover">April 2018</p>
             <p className="whiteOverHover">May 2018</p>
             <p className="whiteOverHover">June 2018</p>
+            <hr />
           </div>
           <div className="col-sm-2 text-white">
             <h5>Company</h5>
+            <hr />
             <p className="whiteOverHover">About</p>
             <p className="whiteOverHover">Team</p>
             <p className="whiteOverHover">Terms & Policies</p>
             <p className="whiteOverHover">Contact Us</p>
+            <hr />
           </div>
           <div className="col-sm-2 text-white">
             <h5>Contact Info</h5>
+            <hr />
             <p className="text-white">Address</p>
             <p>New York - 2398 10 Hadson Carl Street</p>
             <p className="text-white">Telephone</p>
             <p>+1 232 305 3930</p>
             <p className="text-white">Email</p>
             <p>info@yourdomain.com</p>
+            <hr />
           </div>
         </div>
         <p className="text-center copyRight">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+  const showSideBar = () => {
+    setIsSidebarVisible(true);
+  };
+
+  const hideSideBar = () => {
+    setIsSidebarVisible(false);
+  };
+
   return (
     <div>
       <div className="NavBar py-2">
@@ -10,30 +20,70 @@ const Header = () => {
             <h3 className="text-white">JobStart</h3>
           </li>
           <div className="spaceBetweenNavItems"></div>
-          <NavLink to="/" className="text-black pgf">
+          <NavLink to="/" className="hideOnMobile text-black pgf">
             Home
           </NavLink>
-          <NavLink to="/about" className="shadeOverHover pgf">
+          <NavLink to="/about" className="hideOnMobile shadeOverHover pgf">
             About
           </NavLink>
-          <NavLink to="/blog" className="shadeOverHover pgf">
+          <NavLink to="/blog" className="hideOnMobile shadeOverHover pgf">
             Blog
           </NavLink>
-          <NavLink to="/contact" className="shadeOverHover pgf">
+          <NavLink to="/contact" className="hideOnMobile shadeOverHover pgf">
             Contact
           </NavLink>
           <li>
             <NavLink to="/loginform">
-              <button type="button" className="btn btn-info text-white">
+              <button
+                type="button"
+                className="hideOnMobile btn btn-info text-white"
+              >
                 Login
               </button>
             </NavLink>
           </li>
-          <li className="mx-2">
+          <li className="hideOnMobile mx-2">
             <input className="searchBar" type="text" placeholder="Search..." />
             <button className="searchButton">Search</button>
           </li>
+          <button onClick={showSideBar} className="display-none hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
         </ul>
+        {isSidebarVisible && (
+          <ul className="sideBar">
+            <p className="closeSideBar" onClick={hideSideBar}>
+              X
+            </p>
+            <li>
+              <h3 className="text-white">JobStart</h3>
+            </li>
+            <div className="spaceBetweenNavItems"></div>
+            <NavLink to="/" className="text-black pgf">
+              <li>Home</li>
+            </NavLink>
+            <NavLink to="/about" className="shadeOverHover pgf">
+              <li>About</li>
+            </NavLink>
+            <NavLink to="/blog" className="shadeOverHover pgf">
+              <li>Blog</li>
+            </NavLink>
+            <NavLink to="/contact" className="shadeOverHover pgf">
+              <li>Contact</li>
+            </NavLink>
+            <li>
+              <NavLink to="/loginform">
+                <li>
+                  <button type="button" className="btn btn-info text-white">
+                    Login
+                  </button>
+                </li>
+              </NavLink>
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
