47490e87ccf0cb469d6a04793796d7349e832031
turanlz <turanhasan013@gmail.com>
2024-12-17 16:38:47 +0400

    update design

--- src/components/HomeBanner/HeroBg.jsx
+++ src/components/HomeBanner/HeroBg.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../css/HeroBg.css";
 import Dropdown from "react-bootstrap/Dropdown";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
 const HeroBg = () => {
   return (
@@ -10,67 +10,24 @@ const HeroBg = () => {
     <div className='hero-bg'>
       {/* HERO CONATINER */}
       <div className='hero-container container-lg'>
-        {/* HERO CONTENT  */}
-        <div className='hero-content'>
-          {/* HERO TITLE  */}
-          <div className='hero-title'>
-            <h1 className='text-capitalize'>
-              <span>largest job </span>
-              Website on The Net
-            </h1>
-          </div>
+        {/* HERO TEXT CONTAINER */}
+        <div className='hero-text-box'>
+          <h1>
+            Get <span>2500+</span>
+          </h1>
+          <h2>Best Online Courses From Acadia</h2>
+          <p>Acquire global knowledge and build your professional skills</p>
+          <a href=''>
+            Find New Job <i className='fa-solid fa-arrow-right'></i>
+          </a>
+        </div>
 
-          {/* HERO JOB SEARCH  */}
-          <div className='hero-job-search'>
-            {/* JOB FIND  */}
-            <div className='hero-job-find'>
-              <ul>
-                <li className='text-uppercase'>
-                  <a href='#'>Find a job</a>
-                </li>
-                <li className='text-uppercase'>
-                  <a href='#'>Find a candidate</a>
-                </li>
-              </ul>
-            </div>
-
-            {/* JOB SEARCH INPUT  */}
-            <div className='row job-input-wrapper d-flex justify-content-between align-items-center'>
-              <div className='col-md-3'>
-                <input type='text' placeholder='eg. Web Developer' />
-              </div>
-              <div className='col-md-3'>
-                <Dropdown className='search-dropdown'>
-                  <Dropdown.Toggle variant='success' id='dropdown-basic'>
-                    Category
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
-                    <Dropdown.Item href='#/action-2'>
-                      Another action
-                    </Dropdown.Item>
-                    <Dropdown.Item href='#/action-3'>
-                      Something else
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-
-              {/* LOCATION */}
-              <div className='col-md-3 select-location'>
-                <Form.Select className="form-select" aria-label='Default select example'>
-                  <option>Location</option>
-                  <option value='1'>Azerbaijan, Baku</option>
-                  <option value='2'>Turkey, Istanbul</option>
-                  <option value='3'>Italy, Milan</option>
-                </Form.Select>
-              </div>
-
-              {/* SEARCH BUTTON */}
-              <div className='col-md-3 d-flex job-search-btn d-flex justify-content-center align-items-center'>
-                <a href=''>Search</a>
-              </div>
+        {/* HERO BACKGROUND IMAGE */}
+        <div className="hero-image">
+          <div className="main-image">
+            <div className="animate"></div>
+            <div className="bg-image">
+            <img src="https://html.hixstudio.net/acadia-prev/acadia/assets/img/hero/home-6/hero-6-thumb.png" alt="" />
             </div>
           </div>
         </div>
--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../../css/JobCard.css";
 import AspectRatio from "@mui/joy/AspectRatio";
 import Button from "@mui/joy/Button";
@@ -7,8 +7,29 @@ import CardContent from "@mui/joy/CardContent";
 import IconButton from "@mui/joy/IconButton";
 import Typography from "@mui/joy/Typography";
 import BookmarkAdd from "@mui/icons-material/BookmarkAddOutlined";
+import axios from "axios";
+
 
 const JobCard = () => {
+  const [users, setUsers] = useState([]);
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [jobsCard, setJobsCard] = useState([]);
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  useEffect(() => {
+     const fetchData = async () => {
+      try {
+        const response = await axios("http://localhost:5000/jobCard")
+        setJobsCard(response.data);
+      } catch (error) {
+        console.error('Error', error);
+      }
+     }
+     fetchData();
+  }, []);
+
   return (
     <div className='jobcard-main container-lg'>
       {/* {JOB CARD TITLE} */}
@@ -18,326 +39,44 @@ const JobCard = () => {
 
       {/* JOB CARD FULL CONTENT */}
       <div className='jobcard-content d-flex justify-content-between'>
-        <Card sx={{ width: 320 }}>
-          <div>
-            <Typography level='title-lg'>Frontend Developer</Typography>
-            <Typography level='body-sm'>April 24 to May 02, 2021</Typography>
-            <IconButton
-              aria-label='bookmark Bahamas Islands'
-              variant='plain'
-              color='neutral'
-              size='sm'
-              sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
-            >
-              <BookmarkAdd />
-            </IconButton>
-          </div>
-          <AspectRatio minHeight='120px' maxHeight='200px'>
-            <img
-              src='https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-              srcSet='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x'
-              loading='lazy'
-              alt=''
-            />
-          </AspectRatio>
-          <CardContent orientation='horizontal'>
+        {jobsCard.map((card) => (
+            <Card sx={{ width: 300 }}>
             <div>
-              <Typography level='body-xs'>Total price:</Typography>
-              <Typography sx={{ fontSize: "lg", fontWeight: "lg" }}>
-                $2,900
-              </Typography>
+              <Typography level='title-lg'>{card.title}</Typography>
+              <Typography level='body-sm'>{card.author}</Typography>
+              <IconButton
+                aria-label='bookmark Bahamas Islands'
+                variant='plain'
+                color='neutral'
+                size='sm'
+                sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
+              >
+                <BookmarkAdd />
+              </IconButton>
             </div>
-            <Button
-              variant='solid'
-              size='md'
-              color='primary'
-              aria-label='Explore Bahamas Islands'
-              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-            >
-              Send CV
-            </Button>
-          </CardContent>
-        </Card>
-        <Card sx={{ width: 320 }}>
-          <div>
-            <Typography level='title-lg'>Help Desk</Typography>
-            <Typography level='body-sm'>January 4 to April 13, 2021</Typography>
-            <IconButton
-              aria-label='bookmark Bahamas Islands'
-              variant='plain'
-              color='neutral'
-              size='sm'
-              sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
-            >
-              <BookmarkAdd />
-            </IconButton>
-          </div>
-          <AspectRatio minHeight='120px' maxHeight='200px'>
-            <img
-              src='https://www.sydle.com/blog/assets/post/help-desk-types-632b8ce3d4d20a1f70c0f02e/help-desk-icon.png'
-              srcSet='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x'
-              loading='lazy'
-              alt=''
-            />
-          </AspectRatio>
-          <CardContent orientation='horizontal'>
-            <div>
-              <Typography level='body-xs'>Total price:</Typography>
-              <Typography sx={{ fontSize: "lg", fontWeight: "lg" }}>
-                $1,300
-              </Typography>
-            </div>
-            <Button
-              variant='solid'
-              size='md'
-              color='primary'
-              aria-label='Explore Bahamas Islands'
-              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-            >
-              Explore
-            </Button>
-          </CardContent>
-        </Card>
-        <Card sx={{ width: 320 }}>
-          <div>
-            <Typography level='title-lg'>Backend Developer</Typography>
-            <Typography level='body-sm'>June 24 to August 14, 2023</Typography>
-            <IconButton
-              aria-label='bookmark Bahamas Islands'
-              variant='plain'
-              color='neutral'
-              size='sm'
-              sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
-            >
-              <BookmarkAdd />
-            </IconButton>
-          </div>
-          <AspectRatio minHeight='120px' maxHeight='200px'>
-            <img
-              src='https://images.unsplash.com/photo-1630091003936-aea522c1e8c3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-              srcSet='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x'
-              loading='lazy'
-              alt=''
-            />
-          </AspectRatio>
-          <CardContent orientation='horizontal'>
-            <div>
-              <Typography level='body-xs'>Total price:</Typography>
-              <Typography sx={{ fontSize: "lg", fontWeight: "lg" }}>
-                $2,900
-              </Typography>
-            </div>
-            <Button
-              variant='solid'
-              size='md'
-              color='primary'
-              aria-label='Explore Bahamas Islands'
-              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-            >
-              Explore
-            </Button>
-          </CardContent>
-        </Card>
-        <Card sx={{ width: 320 }}>
-          <div>
-            <Typography level='title-lg'>UX&UI Designer</Typography>
-            <Typography level='body-sm'>April 24 to May 02, 2021</Typography>
-            <IconButton
-              aria-label='bookmark Bahamas Islands'
-              variant='plain'
-              color='neutral'
-              size='sm'
-              sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
-            >
-              <BookmarkAdd />
-            </IconButton>
-          </div>
-          <AspectRatio minHeight='120px' maxHeight='200px'>
-            <img
-              src='https://plus.unsplash.com/premium_photo-1661412938808-a0f7be3c8cf1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-              srcSet='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x'
-              loading='lazy'
-              alt=''
-            />
-          </AspectRatio>
-          <CardContent orientation='horizontal'>
-            <div>
-              <Typography level='body-xs'>Total price:</Typography>
-              <Typography sx={{ fontSize: "lg", fontWeight: "lg" }}>
-                $2,100
-              </Typography>
-            </div>
-            <Button
-              variant='solid'
-              size='md'
-              color='primary'
-              aria-label='Explore Bahamas Islands'
-              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-            >
-              Explore
-            </Button>
-          </CardContent>
-        </Card>
-        <Card sx={{ width: 320 }}>
-          <div>
-            <Typography level='title-lg'>Cybersecurity Analyst</Typography>
-            <Typography level='body-sm'>April 24 to May 02, 2021</Typography>
-            <IconButton
-              aria-label='bookmark Bahamas Islands'
-              variant='plain'
-              color='neutral'
-              size='sm'
-              sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
-            >
-              <BookmarkAdd />
-            </IconButton>
-          </div>
-          <AspectRatio minHeight='120px' maxHeight='200px'>
-            <img
-              src='https://www.globalitsvcs.com/wp-content/uploads/Information-Cybersecurity-Analysts.jpg'
-              srcSet='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x'
-              loading='lazy'
-              alt=''
-            />
-          </AspectRatio>
-          <CardContent orientation='horizontal'>
-            <div>
-              <Typography level='body-xs'>Total price:</Typography>
-              <Typography sx={{ fontSize: "lg", fontWeight: "lg" }}>
-                $5,700
-              </Typography>
-            </div>
-            <Button
-              variant='solid'
-              size='md'
-              color='primary'
-              aria-label='Explore Bahamas Islands'
-              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-            >
-              Explore
-            </Button>
-          </CardContent>
-        </Card>
-        <Card sx={{ width: 320 }}>
-          <div>
-            <Typography level='title-lg'>Robotics Teacher</Typography>
-            <Typography level='body-sm'>April 24 to May 02, 2021</Typography>
-            <IconButton
-              aria-label='bookmark Bahamas Islands'
-              variant='plain'
-              color='neutral'
-              size='sm'
-              sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
-            >
-              <BookmarkAdd />
-            </IconButton>
-          </div>
-          <AspectRatio minHeight='120px' maxHeight='200px'>
-            <img
-              src='https://gtectvm.com/wp-content/uploads/2023/06/home-made-robot-desk-1024x683.jpg'
-              srcSet='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x'
-              loading='lazy'
-              alt=''
-            />
-          </AspectRatio>
-          <CardContent orientation='horizontal'>
-            <div>
-              <Typography level='body-xs'>Total price:</Typography>
-              <Typography sx={{ fontSize: "lg", fontWeight: "lg" }}>
-                $3,400
-              </Typography>
-            </div>
-            <Button
-              variant='solid'
-              size='md'
-              color='primary'
-              aria-label='Explore Bahamas Islands'
-              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-            >
-              Explore
-            </Button>
-          </CardContent>
-        </Card>
-        <Card sx={{ width: 320 }}>
-          <div>
-            <Typography level='title-lg'>System Administrator</Typography>
-            <Typography level='body-sm'>April 24 to May 02, 2021</Typography>
-            <IconButton
-              aria-label='bookmark Bahamas Islands'
-              variant='plain'
-              color='neutral'
-              size='sm'
-              sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
-            >
-              <BookmarkAdd />
-            </IconButton>
-          </div>
-          <AspectRatio minHeight='120px' maxHeight='200px'>
-            <img
-              src='https://180engineering.com/wp-content/uploads/2022/10/A-Day-In-The-Life-Of-An-IT-Systems-Administrator.jpg'
-              srcSet='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x'
-              loading='lazy'
-              alt=''
-            />
-          </AspectRatio>
-          <CardContent orientation='horizontal'>
-            <div>
-              <Typography level='body-xs'>Total price:</Typography>
-              <Typography sx={{ fontSize: "lg", fontWeight: "lg" }}>
-                $2,900
-              </Typography>
-            </div>
-            <Button
-              variant='solid'
-              size='md'
-              color='primary'
-              aria-label='Explore Bahamas Islands'
-              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-            >
-              Explore
-            </Button>
-          </CardContent>
-        </Card>
-        <Card sx={{ width: 320 }}>
-          <div>
-            <Typography level='title-lg'>Mobile Developer</Typography>
-            <Typography level='body-sm'>April 24 to May 02, 2021</Typography>
-            <IconButton
-              aria-label='bookmark Bahamas Islands'
-              variant='plain'
-              color='neutral'
-              size='sm'
-              sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
-            >
-              <BookmarkAdd />
-            </IconButton>
-          </div>
-          <AspectRatio minHeight='120px' maxHeight='200px'>
-            <img
-              src='https://www.techbeatly.com/wp-content/uploads/2022/02/image-1024x682.jpeg'
-              srcSet='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x'
-              loading='lazy'
-              alt=''
-            />
-          </AspectRatio>
-          <CardContent orientation='horizontal'>
-            <div>
-              <Typography level='body-xs'>Total price:</Typography>
-              <Typography sx={{ fontSize: "lg", fontWeight: "lg" }}>
-                $2,900
-              </Typography>
-            </div>
-            <Button
-              variant='solid'
-              size='md'
-              color='primary'
-              aria-label='Explore Bahamas Islands'
-              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-            >
-              Send CV
-            </Button>
-          </CardContent>
-        </Card>
+            <AspectRatio minHeight='120px' maxHeight='200px'>
+              <img src={card.image} alt="" />
+            </AspectRatio>
+            <CardContent orientation='horizontal'>
+              <div>
+                <Typography level='body-xs'>Total price:</Typography>
+                <Typography sx={{ fontSize: "lg", fontWeight: "lg" }}>
+                  {card.price}
+                </Typography>
+              </div>
+              <Button
+                variant='solid'
+                size='md'
+                color='primary'
+                aria-label='Explore Bahamas Islands'
+                sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
+              >
+                Send CV
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
+        
       </div>
     </div>
   );
